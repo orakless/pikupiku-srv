77eb485620e4049ebf8459703f9540009a08abad
Eva G. <orakle@tutamail.com>
2025-05-07 14:33:30 +0200

    initial commit

---
+++ src/main/java/com/pikupikusrv/PikupikuSrvApplication.java
@@ -0,0 +1,13 @@
+package com.pikupikusrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikupikuSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PikupikuSrvApplication.class, args);
+    }
+
+}
